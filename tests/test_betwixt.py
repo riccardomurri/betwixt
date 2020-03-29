6b5e9ec02eb4a6a963280e1625c770a01ba6333f
Riccardo Murri <riccardo.murri@gmail.com>
2020-03-29 21:10:35 +0200

    Split off tests into file `tests/test_betwixt.py`.

--- tests/test_betwixt.py
+++ tests/test_betwixt.py
@@ -1,6 +1,83 @@
+#
+# Test suite for the `betwixt` package
+#
 
-from betwixt import main
+from fnmatch import fnmatch
+
+from betwixt import *
 
 
-def test_main():
-    pass
+class TestDelimitedInfixOperator(object):
+    """
+    Test operator creation via `*DelimitedInfixOperator` classes.
+    """
+
+    def test_double_star_delimited_operator(self):
+        matches = DoubleStarDelimitedInfixOperator(fnmatch)
+        assert ('foo.txt' **matches** '*.txt')
+
+    def test_star_delimited_operator(self):
+        matches = StarDelimitedInfixOperator(fnmatch)
+        assert ('foo.txt' *matches* '*.txt')
+
+    def test_plus_delimited_operator(self):
+        matches = PlusDelimitedInfixOperator(fnmatch)
+        assert ('foo.txt' +matches+ '*.txt')
+
+    def test_bar_delimited_operator(self):
+        matches = BarDelimitedInfixOperator(fnmatch)
+        assert ('foo.txt' |matches| '*.txt')
+
+    def test_caret_delimited_operator(self):
+        matches = CaretDelimitedInfixOperator(fnmatch)
+        assert ('foo.txt' ^matches^ '*.txt')
+
+    def test_slash_delimited_operator(self):
+        matches = SlashDelimitedInfixOperator(fnmatch)
+        assert ('foo.txt' /matches/ '*.txt')
+
+    def test_double_slash_delimited_operator(self):
+        matches = DoubleSlashDelimitedInfixOperator(fnmatch)
+        assert ('foo.txt' //matches// '*.txt')
+
+    def test_angle_delimited_operator(self):
+        matches = AngleDelimitedInfixOperator(fnmatch)
+        assert ('foo.txt' <<matches>> '*.txt')
+
+
+class Test_make_infix_operator(object):
+    """
+    Test operator creation via `make_infix_operator`.
+    """
+
+    def test_star_delimited_operator(self):
+        matches = make_infix_operator(fnmatch, '*')
+        assert ('foo.txt' *matches* '*.txt')
+
+    def test_double_star_delimited_operator(self):
+        matches = make_infix_operator(fnmatch, '**')
+        assert ('foo.txt' **matches** '*.txt')
+
+    def test_slash_delimited_operator(self):
+        matches = make_infix_operator(fnmatch, '/')
+        assert ('foo.txt' /matches/ '*.txt')
+
+    def test_double_slash_delimited_operator(self):
+        matches = make_infix_operator(fnmatch, '//')
+        assert ('foo.txt' //matches// '*.txt')
+
+    def test_angle_delimited_operator(self):
+        matches = make_infix_operator(fnmatch, '<<')
+        assert ('foo.txt' <<matches>> '*.txt')
+
+    def test_plus_delimited_operator(self):
+        matches = make_infix_operator(fnmatch, '+')
+        assert ('foo.txt' +matches+ '*.txt')
+
+    def test_caret_delimited_operator(self):
+        matches = make_infix_operator(fnmatch, '^')
+        assert ('foo.txt' ^matches^ '*.txt')
+
+    def test_bar_delimited_operator(self):
+        matches = make_infix_operator(fnmatch, '|')
+        assert ('foo.txt' |matches| '*.txt')
